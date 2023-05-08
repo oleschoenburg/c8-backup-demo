d2357077c2096af5861088ee318e7293d59b36c2
Ole Schönburg <ole.schoenburg@gmail.com>
2023-05-08 10:44:45 +0200

    Fix dev upgrade script

--- upgrade-dev.sh
+++ upgrade-dev.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-helm -n os-ccs23-dev upgrade ccs23-dev camunda/camunda-platform --values config/prod.yaml --values config/backup.yaml
-kubectl -n os-ccs23-dev apply -f config/workload.yaml
+helm -n os-ccs23-dev upgrade ccs23-dev camunda/camunda-platform --values config/dev.yaml --values config/backup.yaml
 kubectl -n os-ccs23-dev rollout status deploy ccs23-dev-operate &
 kubectl -n os-ccs23-dev rollout status statefulset ccs23-dev-zeebe &
-kubectl -n os-ccs23-dev rollout status -f config/workload.yaml &
 
 wait
